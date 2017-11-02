26e7ffe76265a31a0f0b15fdc31a7315f7cd8d32
jlee187 <jonathan.llee.8@gmail.com>
2017-11-02 15:17:50 -0400

    JS - decides where to place x/o choice based on turn
    Add functionality to cover X and O arrays that hold their respective values

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -15,17 +15,19 @@ $(() => {
 
 const clickEvents = require('./events')
 
-// let checkCheck = 0 // see if the click event is registering properly... can't increment appropriately
-let whoseTurn = 0
-
 // On document ready
 $(() => {
+// let checkCheck = 0 // see if the click event is registering properly... can't increment appropriately
+
+  let whoseTurn = 1
+  console.log(whoseTurn)
+
   $('#cell-zero').on('submit', clickEvents.onGetBooks) // click function to register click with API... must record in JS too
 
   let choice = 0
 
   $('#cell-zero').on('click', function (event) {
-    // whoseTurn += 1 // scope issue... child can access parent but parent cannot access local
+    whoseTurn += 1 // scope issue... child can access parent but parent cannot access local
     choice = ($(this).val()) + 1
     return choice
     // console.log(whoseTurn)
@@ -34,9 +36,56 @@ $(() => {
   })
 
   $('#cell-one').on('click', function (event) {
-    console.log('clicked', $(this).text())
+    $('#cell-one').text(function () {
+      if (whoseTurn % 2 === 0) {
+        return 'X'
+      } else {
+        return 'O'
+      }
+    })
+  })
+
+  $('#cell-two').on('click', function (event) {
+    $('#cell-two').text(function () {
+      if (whoseTurn % 2 === 0) {
+        return 'X'
+      } else {
+        return 'O'
+      }
+    })
+  })
+
+  $('#cell-three').on('click', function (event) {
+    $('#cell-three').text(function () {
+      if (whoseTurn % 2 === 0) {
+        return 'X'
+      } else {
+        return 'O'
+      }
+    })
+  })
+
+  $('#cell-four').on('click', function (event) {
+    $('#cell-four').text(function () {
+      if (whoseTurn % 2 === 0) {
+        return 'X'
+      } else {
+        return 'O'
+      }
+    })
+  })
+
+  $('#cell-five').on('click', function (event) {
+    $('#cell-five').text(function () {
+      if (whoseTurn % 2 === 0) {
+        return 'X'
+      } else {
+        return 'O'
+      }
+    })
   })
 })
+
 // Attempted to interate through click events... probably going to have to have 9 event handlers
 // $('.board').click(function (event) {
 //   event.stopPropagation()
