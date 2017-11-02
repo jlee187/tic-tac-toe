13d478ca4b08831fbf62747547c048f62c60cc0f
jlee187 <jonathan.llee.8@gmail.com>
2017-11-02 13:47:30 -0400

    CSS JS
    CSS: Centered box
    JS: push unique keys into array

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -20,12 +20,15 @@ let whoseTurn = 0
 
 // On document ready
 $(() => {
-  // $('#cell-zero').on('submit', clickEvents.onGetBooks) // click function to register click with API... must record in JS too
+  $('#cell-zero').on('submit', clickEvents.onGetBooks) // click function to register click with API... must record in JS too
+
+  let choice = 0
 
   $('#cell-zero').on('click', function (event) {
-    whoseTurn += 1 // scope issue... child can access parent but parent cannot access local
-    console.log(whoseTurn)
-    console.log('clicked', $(this).text())
+    // whoseTurn += 1 // scope issue... child can access parent but parent cannot access local
+    choice = ($(this).val()) + 1
+    return choice
+    // console.log(whoseTurn)
     // checkCheck += 1
     // console.log(checkCheck)
   })
@@ -34,3 +37,15 @@ $(() => {
     console.log('clicked', $(this).text())
   })
 })
+// Attempted to interate through click events... probably going to have to have 9 event handlers
+// $('.board').click(function (event) {
+//   event.stopPropagation()
+//   console.log(10)
+// })
+//
+
+// $('#cell-').on('click', function (event) {
+//   console.log('clicked', $(this).text())
+// }).each(function () {
+//   console.log('clicked', $(this).text())
+// })
