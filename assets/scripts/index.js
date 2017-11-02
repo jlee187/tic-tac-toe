16ebcbc2ffba1bd1ff1998cd02db300f9a26e44a
jlee187 <jonathan.llee.8@gmail.com>
2017-11-02 10:48:56 -0400

    Add click handler for cells: zero and one
    Add respective click handlers; attempted to increment "whoseTurn" by
    every click, but had local and global scope issues... try to create for
    loop that increments through click handlers?

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -15,7 +15,22 @@ $(() => {
 
 const clickEvents = require('./events')
 
+// let checkCheck = 0 // see if the click event is registering properly... can't increment appropriately
+let whoseTurn = 0
+
 // On document ready
 $(() => {
-  $('#cell-zero').on('submit', clickEvents.onGetBooks) // click function to register click with API... must record in JS too
+  // $('#cell-zero').on('submit', clickEvents.onGetBooks) // click function to register click with API... must record in JS too
+
+  $('#cell-zero').on('click', function (event) {
+    whoseTurn += 1 // scope issue... child can access parent but parent cannot access local
+    console.log(whoseTurn)
+    console.log('clicked', $(this).text())
+    // checkCheck += 1
+    // console.log(checkCheck)
+  })
+
+  $('#cell-one').on('click', function (event) {
+    console.log('clicked', $(this).text())
+  })
 })
